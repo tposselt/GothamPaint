020bf7f4a9ac82ebcca0d9470920abd4dbe59eef
NeumontAGrover <agrover@student.neumont.edu>
2026-02-04 09:14:41 -0700

    Added Raylib Project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Gotham Paint");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  Raylib.ClearBackground(Raylib.GetColor(0x2C3E50FF));
+  Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
